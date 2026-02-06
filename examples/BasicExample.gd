e54d95a8d7cf56f7e9fa31029eb39762048f17bc
joseph <joseph96501@gmail.com>
2026-02-05 20:33:53 -0500

    Playing TEST animation

--- examples/BasicExample.gd
+++ examples/BasicExample.gd
@@ -2,9 +2,12 @@ extends Gist
 class_name BasicExample
 
 @onready var TEST: Animatable = Of(
-	IT(skeleton.body.position.x)
+	IT(skeleton.Bob.position.x)
 		.by(5.0).over(1.0)
 		.then()
 		.to(10.0).over(2.0)
 		.end()
 )
+
+func _ready():
+	TEST.play()
