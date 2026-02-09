7798d292a3caa11d49d11d1a137a8b18e2e737a4
joseph <joseph96501@gmail.com>
2026-02-09 15:44:40 -0500

    BasicExample changes

--- examples/BasicExample/BasicExample.gd
+++ examples/BasicExample/BasicExample.gd
@@ -4,14 +4,16 @@ class_name BasicExample
 @onready var bob: Bobert = $Bob
 
 func _ready():
-	BasicExample.TEST(bob).play()
+	BasicExample.TEST(bob).play() \
+		.speed(1.0)
 
 # In another file
 static func TEST(skeleton: Bobert) -> Gist.Animatable: return \
 	Gist.Of(
 		Gist.IT(skeleton.Body.scale.x)
-			.by(5.0).over(1.0)
+			.by(20.0).over(0.5)
 			.then()
 			.to(10.0).over(2.0)
 			.end()
-	)
+	) \
+		.reverse(false)
